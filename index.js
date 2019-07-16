a5d04dd133a8700aba9a258a74019b11383b407d
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-16 10:56:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function Scooter(year, color, model) {
   this.color = model
 }
 
+let samson = new Scooter(2015, "red", "swoosh")
+
 /*describe('Scooter constructor', function(){
     it("should create a new scooter with three properties", function(){
       let samson = new Scooter(2015, "red", "swoosh");
