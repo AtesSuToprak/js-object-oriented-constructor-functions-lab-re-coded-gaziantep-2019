30aaf705812b6c169c9ae42f5c8d5fff9e859d1e
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-16 11:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,17 +15,7 @@ function Driver (name, age, experience) {
 let allison = new Driver("Allison", "16 years", "2 years");
 
 
-/*describe('Driver constructor', function(){
-    it("should create a new driver with three properties", function(){
-      let allison = new Driver("Allison", "16 years", "2 years");
-      expect(allison).to.be.an.instanceof(Driver);
-      expect(allison.name).to.equal("Allison");
-      expect(allison.age).to.equal("16 years");
-      expect(allison.experience).to.equal("2 years");
-    });
-  });
-
-  describe('PickupLocation constructor', function(){
+/*describe('PickupLocation constructor', function(){
     it("should create a new PickupLocation with two properties", function(){
       let poshHotel = new PickupLocation("123 Broadway", "New York City");
       expect(poshHotel).to.be.an.instanceof(PickupLocation);
